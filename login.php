6129763207d8fd99e3d404300ef8321862699781
amircolgang <parham38385747@gmail.com>
2025-06-15 23:53:11 +0330

    Amircolgang: changes on 2025-06-15 23:53:11

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
-<?php
-    <form action="index.php">
-        
-    </form>
-?>
+<form action="index.php">
+    <input type="text" placeholder="name">
+    <input type="text" placeholder="username">
+    <input type="text" placeholder="email">
+    <input type="text" placeholder="password">
+</form>
