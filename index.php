0c6cb28aca06a73e178425ed8e6c1e0dca51ec73
amircolgang <parham38385747@gmail.com>
2025-06-16 00:05:55 +0330

    Amircolgang: changes on 2025-06-16 00:05:55

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST" ){
-        
+        if($_POST["email"] == "admin" && $_POST["pass"] == "123456"){
+            //
+        } else {
+            header("Location : login.php")
+        }
     }
 
 ?>
